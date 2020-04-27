ce25a71bf861b880569baeead22257478557c831
Yohanes Septiawan <yohanes.septiawan@gmail.com>
2020-04-27 16:24:07 +0800

    second commit

--- components/List.js
+++ components/List.js
@@ -1,62 +1,58 @@
 import React from 'react';
-import { View, Text, FlatList} from 'react-native';
+import { View, Text, FlatList, Image} from 'react-native';
 
 class List extends React.Component{
     constructor(){
         super();
         this.state = {
-            students: [
-                {
-                    id: 1,
-                    name: 'Yohanes',
-                    faculty: 'Computer Science'
-                },
-                {
-                    id: 2,
-                    name: 'Jono',
-                    faculty: 'Business'
-                },
-                {
-                    id: 3,
-                    name: 'Joni',
-                    faculty: 'Nurse'
-                },
-                {
-                    id: 4,
-                    name: 'Jona',
-                    faculty: 'Secretary'
-                },
-            ],
+            users: [],
             refreshing: false
         }
     }
 
     renderItem = ({ item }) => (
-        <View style={{ padding: 20, borderBottomWidth: 1, borderBottomColor: '#000'}}>
-            <Text>Name: {item.name}</Text>
-            <Text>Faculty: {item.faculty}</Text>
+        <View style={{ flex: 1, flexDirection: 'row', padding: 20, borderBottomWidth: 1, borderBottomColor: '#000'}}>
+            <Image 
+                source={{ uri : 'https://robohash.org/${item.id}?set=set1'}}
+                style={{ width: 50, height: 50 }}
+            />
+            <View>
+                <Text>Name: {item.name}</Text>
+                <Text>Username: {item.username}</Text>
+            </View>
         </View>
     )
 
     onRefresh = () => {
+        this.getDataApi();
+
+    }
+
+    componentDidMount = () => {
+        this.getDataApi();
+    }
+
+    getDataApi = async () => {
         this.setState({ refreshing: true })
-        const data = this.state.students.concat({ id: 5, name: 'Jordan', faculty: 'Computer Science'})
-        this.setState({
-            refreshing: false,
-            students: data
-        })
+        // fetch('http://jsonplaceholder.typicode.com/users')
+        //     .then(response => response.json())
+        //     .then(json => this.setState({ users: json, refreshing: false }))
+        
+        const response = await fetch('http://jsonplaceholder.typicode.com/users');
+        const json = await response.json();
+        this.setState({ users: json, refreshing: false })
     }
    
     render(){
-
         return (
             <View>
                <FlatList
-                   data={this.state.students}
+                   data={this.state.users}
                    keyExtractor={item => item.id.toString()}
                    renderItem={this.renderItem}
                    refreshing={this.state.refreshing}
                    onRefresh={this.onRefresh}
+                   showsVerticalScrollIndicator={false}
                 />
             </View>
         )
